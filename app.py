82d02066dcfc553cac75cb6392d5335ff359fe31
joelberlin174 <joelberlin174@gmail.com>
2026-03-25 21:04:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+history = []   # shared history for all users
+
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    global history
+    data = request.json
+    history.append(data['calc'])
+
+    # limit history to last 10
+    history = history[-10:]
+    return jsonify({"status": "ok"})
+
+@app.route('/history')
+def get_history():
+    return jsonify(history)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
